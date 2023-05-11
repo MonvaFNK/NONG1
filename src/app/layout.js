ed1c7a6475964113425723a58e8e3e88e50e13d9
Santiago Diaz Monvale <105252205+MonvaFNK@users.noreply.github.com>
2023-05-11 01:55:45 -0300

    hotfix 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,15 +3,10 @@ import './globals.css'
 import './stylesheets'
 import Header from './../components/Header/Header';
 import Footer from './../components/Footer/Footer';
-
 import ContactContext from './../context/ContactFormContext';
 import React, { useState } from 'react';
 import Contacto from './../components/contact/Contact';
 
-export const metadata = {
-  title: 'Agencia Hispana',
-  description: 'Servicios legales de Agencia Hispana.',
-}
 
 export default function RootLayout({ children }) {
   const [showContact, setShowContact] = useState(false);
